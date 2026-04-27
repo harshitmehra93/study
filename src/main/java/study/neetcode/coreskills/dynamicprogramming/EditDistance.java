6eb2d01df14f566860e3136145b1e6e046494226
jasonbourne_sysa <sysa202305@sysanalytix.com>
2026-04-27 21:36:01 +0530

    rewriting dp algos

--- src/main/java/study/neetcode/coreskills/dynamicprogramming/EditDistance.java
+++ src/main/java/study/neetcode/coreskills/dynamicprogramming/EditDistance.java
@@ -1,73 +1,57 @@
-package study.neetcode.coreskills.dynamicprogramming;
+    package study.neetcode.coreskills.dynamicprogramming;
 
-public class EditDistance {
+    import java.util.Arrays;
 
-    //    Problem: Edit Distance
-    //
-    //    You are given two strings:
-    //    word1
-    //    word2
-    //
-    //    You can perform the following operations on word1:
-    //    1. Insert a character
-    //    2. Delete a character
-    //    3. Replace a character
-    //
-    //    Return the minimum number of operations required to convert word1 into word2.
-    //
-    //    Examples
-    //    Input:
-    //    word1 = "horse"
-    //    word2 = "ros"
-    //
-    //    Output: 3
-    //
-    //    Explanation:
-    //    horse → rorse   replace h with r
-    //    rorse → rose    delete r
-    //    rose  → ros     delete e
-    public int editDistance(String current, String target) {
-        return helper(current, target, 0, 0);
+    public class EditDistance {
+
+        //    Problem: Edit Distance
+        //
+        //    You are given two strings:
+        //    word1
+        //    word2
+        //
+        //    You can perform the following operations on word1:
+        //    1. Insert a character
+        //    2. Delete a character
+        //    3. Replace a character
+        //
+        //    Return the minimum number of operations required to convert word1 into word2.
+        //
+        //    Examples
+        //    Input:
+        //    word1 = "horse"
+        //    word2 = "ros"
+        //
+        //    Output: 3
+        //
+        //    Explanation:
+        //    horse → rorse   replace h with r
+        //    rorse → rose    delete r
+        //    rose  → ros     delete e
+        Integer[][] memo;
+        public int editDistance(String word1, String word2) {
+            memo = new Integer[word1.length()+1][word2.length()+1];
+            return helper(word1, word2, 0, 0);
+        }
+
+        private int helper(String word, String target, int i, int j) {
+            if(i==word.length()) return target.length()-j;
+            if(j==target.length()) return word.length()-i;
+            if(memo[i][j]!=null) return memo[i][j];
+
+            if(word.charAt(i)==target.charAt(j)) {
+                return memo[i][j]=helper(word, target, i+1, j+1);
+            }
+
+            int replace = helper(word, target, i+1, j+1);
+            int insert = helper(word, target, i, j+1);
+            int remove = helper(word, target, i+1, j);
+
+            int min=Integer.MAX_VALUE;
+            min = Math.min(min, replace);
+            min = Math.min(min, insert);
+            min = Math.min(min, remove);
+
+            return memo[i][j]=1+min;
+        }
     }
-
-    private int helper(String current, String target, int i, int j) {
-        if (current.equals(target)) return 0;
-        if (i == current.length()) return target.length() - j;
-        if (j == target.length()) return current.length() - i;
-
-        int min = Integer.MAX_VALUE;
-
-        if ((j < target.length() && i < current.length())
-                && (current.charAt(i) == target.charAt(j))) {
-            return helper(current, target, i + 1, j + 1);
-        }
-
-        // replacement
-        int minOfReplacement = Integer.MAX_VALUE;
-        if (i < current.length() && j < target.length()) {
-            minOfReplacement = helper(current, target, i + 1, j + 1);
-            minOfReplacement = minOfReplacement == -1 ? Integer.MAX_VALUE : minOfReplacement;
-        }
-
-        // addition
-        int minOfAddition = Integer.MAX_VALUE;
-        if (j < target.length()) {
-            minOfAddition = helper(current, target, i, j + 1);
-            minOfAddition = minOfAddition == -1 ? Integer.MAX_VALUE : minOfAddition;
-        }
-
-        // removals
-        int minOfRemoval = Integer.MAX_VALUE;
-        if (i < current.length()) {
-            minOfRemoval = helper(current, target, i + 1, j);
-            minOfRemoval = minOfRemoval == -1 ? Integer.MAX_VALUE : minOfRemoval;
-        }
-
-        min = Math.min(minOfReplacement, min);
-        min = Math.min(minOfAddition, min);
-        min = Math.min(minOfRemoval, min);
-
-        if (min == Integer.MAX_VALUE) return -1;
-        return 1 + min;
-    }
-}
