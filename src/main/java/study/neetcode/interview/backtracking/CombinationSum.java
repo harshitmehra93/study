395e053d3d0626a6950b3b6eee8e895d99557ce5
jasonbourne_sysa <sysa202305@sysanalytix.com>
2026-05-24 18:29:52 +0530

    Combination Sum

---
+++ src/main/java/study/neetcode/interview/backtracking/CombinationSum.java
@@ -0,0 +1,56 @@
+package study.neetcode.interview.backtracking;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+Next problem: Combination Sum
+
+You are given an array of distinct integers candidates and a target integer target.
+
+Return all unique combinations of candidates where the chosen numbers sum to target.
+
+You may use the same number unlimited times.
+
+Example:
+
+candidates = [2, 3, 6, 7]
+target = 7
+
+Output:
+
+[
+  [2, 2, 3],
+  [7]
+]
+ */
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new LinkedList<>();
+        tryAllPaths(candidates, target, 0, 0,new LinkedList<Integer>(), result);
+        return result;
+    }
+
+    private void tryAllPaths(
+            int[] candidates,
+            int target,
+            int currentSum,
+            int index,
+            LinkedList<Integer> currentSelectedCandidates,
+            List<List<Integer>> result) {
+        if (currentSum > target) return;
+        if (currentSum == target) result.add(new LinkedList<>(currentSelectedCandidates));
+
+        for (int i = index; i < candidates.length; i++) {
+            currentSelectedCandidates.add(candidates[i]);
+            tryAllPaths(
+                    candidates,
+                    target,
+                    currentSum + candidates[i],
+                    i,
+                    currentSelectedCandidates,
+                    result);
+            currentSelectedCandidates.removeLast();
+        }
+    }
+}
