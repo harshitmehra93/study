a01a0f5969ce12f1670a496c5ef2f6a6c2c86e51
jasonbourne_sysa <sysa202305@sysanalytix.com>
2026-05-26 11:52:41 +0530

    Subset attempt 2

--- src/main/java/study/neetcode/interview/backtracking/Subsets.java
+++ src/main/java/study/neetcode/interview/backtracking/Subsets.java
@@ -1,57 +1,27 @@
 package study.neetcode.interview.backtracking;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-/*
-Problem: Subsets
-
-You are given an integer array nums of unique elements.
-
-Return all possible subsets.
-
-Example:
-
-nums = [1, 2, 3]
-
-Output can be in any order:
-
-[
-  [],
-  [1],
-  [2],
-  [3],
-  [1,2],
-  [1,3],
-  [2,3],
-  [1,2,3]
-]
-
-Method signature:
-
-public List<List<Integer>> subsets(int[] nums)
- */
 public class Subsets {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        traverseAllSubsets(0, nums, result, new LinkedList<Integer>());
+        List<List<Integer>> result = new LinkedList<>();
+        traverseSubsets(0,nums,new LinkedList<>(),result);
         return result;
     }
 
-    private void traverseAllSubsets(
-            int i, int[] nums, List<List<Integer>> result, LinkedList<Integer> currentSet) {
-        if (i >= nums.length) {
-            result.add(new LinkedList<>(currentSet));
+    private void traverseSubsets(int index, int[] nums, LinkedList<Integer> set, List<List<Integer>> result) {
+        if(index==nums.length) {
+            result.add(new LinkedList<>(set));
             return;
         }
 
-        // skip i
-        traverseAllSubsets(i + 1, nums, result, currentSet);
+        // take index
+        set.add(nums[index]);
+        traverseSubsets(index+1,nums,set,result);
+        set.removeLast();
 
-        // take i
-        currentSet.add(nums[i]);
-        traverseAllSubsets(i + 1, nums, result, currentSet);
-        currentSet.removeLast();
+        // skip index
+        traverseSubsets(index+1,nums,set,result);
     }
 }
