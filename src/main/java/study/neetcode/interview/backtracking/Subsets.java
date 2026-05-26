6a46a257f5e23054f59370adce1234482b70aa81
jasonbourne_sysa <sysa202305@sysanalytix.com>
2026-05-26 11:52:54 +0530

    Subset attempt 2

--- src/main/java/study/neetcode/interview/backtracking/Subsets.java
+++ src/main/java/study/neetcode/interview/backtracking/Subsets.java
@@ -6,22 +6,23 @@ import java.util.List;
 public class Subsets {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new LinkedList<>();
-        traverseSubsets(0,nums,new LinkedList<>(),result);
+        traverseSubsets(0, nums, new LinkedList<>(), result);
         return result;
     }
 
-    private void traverseSubsets(int index, int[] nums, LinkedList<Integer> set, List<List<Integer>> result) {
-        if(index==nums.length) {
+    private void traverseSubsets(
+            int index, int[] nums, LinkedList<Integer> set, List<List<Integer>> result) {
+        if (index == nums.length) {
             result.add(new LinkedList<>(set));
             return;
         }
 
         // take index
         set.add(nums[index]);
-        traverseSubsets(index+1,nums,set,result);
+        traverseSubsets(index + 1, nums, set, result);
         set.removeLast();
 
         // skip index
-        traverseSubsets(index+1,nums,set,result);
+        traverseSubsets(index + 1, nums, set, result);
     }
 }
