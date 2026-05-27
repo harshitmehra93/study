9f30d2a6f760b2b8ef8457fa91c5642fa509925f
jasonbourne_sysa <sysa202305@sysanalytix.com>
2026-05-27 16:41:33 +0530

    Subsets 1 attempt 4

--- src/main/java/study/neetcode/interview/backtracking/Subsets.java
+++ src/main/java/study/neetcode/interview/backtracking/Subsets.java
@@ -6,20 +6,20 @@ import java.util.List;
 public class Subsets {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new LinkedList<>();
-        findAllSubsets(0, nums, new LinkedList<Integer>(), result);
+        trySubsets(0, new LinkedList<Integer>(), nums, result);
         return result;
     }
 
-    private void findAllSubsets(
-            int index, int[] nums, LinkedList<Integer> current, List<List<Integer>> result) {
-        result.add(new LinkedList<>(current));
+    private void trySubsets(
+            int index, LinkedList<Integer> set, int[] nums, List<List<Integer>> result) {
+        result.add(new LinkedList<>(set));
 
         for (int i = index; i < nums.length; i++) {
-            current.add(nums[i]);
+            set.add(nums[i]);
 
-            findAllSubsets(i + 1, nums, current, result);
+            trySubsets(i + 1, set, nums, result);
 
-            current.removeLast();
+            set.removeLast();
         }
     }
 }
