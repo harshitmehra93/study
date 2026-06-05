f7e8dea9a181817456aafb0da3cf79858135888b
jasonbourne_sysa <sysa202305@sysanalytix.com>
2026-06-05 19:25:00 +0530

    TwoSumII attempt 2

--- src/main/java/study/neetcode/interview/twopointers/TwoSumII.java
+++ src/main/java/study/neetcode/interview/twopointers/TwoSumII.java
@@ -23,9 +23,7 @@ Explanation:
 public class TwoSumII {
     public int[] twoSum(int[] numbers, int target) {
         int left = 0, right = numbers.length - 1;
-
         while (left < right) {
-            System.out.printf("left=%d, right=%d", left, right);
             int sum = numbers[left] + numbers[right];
             if (sum < target) {
                 left++;
