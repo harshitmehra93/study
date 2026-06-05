762164a66fac9eaf43f5c22c63d46074bc96118f
jasonbourne_sysa <sysa202305@sysanalytix.com>
2026-06-05 20:10:58 +0530

    TrappingRainWater attempt 2

--- src/main/java/study/neetcode/interview/twopointers/TrappingRainWater.java
+++ src/main/java/study/neetcode/interview/twopointers/TrappingRainWater.java
@@ -3,7 +3,8 @@ package study.neetcode.interview.twopointers;
 /*
 Trapping Rain Water — Two Pointers
 
-Given n non-negative integers representing an elevation map, where the width of each bar is 1, compute how much water it can trap after raining.
+Given n non-negative integers representing an elevation map, where the width of each bar is 1,
+compute how much water it can trap after raining.
 
 Example 1
 Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
@@ -22,20 +23,22 @@ Output: 9
  */
 public class TrappingRainWater {
     public int trap(int[] height) {
-        int min = 0;
+        int leftMax = 0;
+        int rightMax = 0;
         int left = 0, right = height.length - 1;
-        int leftMax = 0, rightMax = 0;
         int water = 0;
         while (left < right) {
-            leftMax = Math.max(leftMax, height[left]);
-            rightMax = Math.max(rightMax, height[right]);
-            int minWaterLevel = Math.min(leftMax, rightMax);
+            int leftHeight = height[left];
+            int rightHeight = height[right];
+            leftMax = Math.max(leftMax, leftHeight);
+            rightMax = Math.max(rightMax, rightHeight);
+
+            int minOfTwoMaxes = Math.min(leftMax, rightMax);
             if (leftMax <= rightMax) {
-                water += minWaterLevel - height[left];
+                water += minOfTwoMaxes - leftHeight;
                 left++;
-            }
-            if (rightMax < leftMax) {
-                water += minWaterLevel - height[right];
+            } else if (rightMax < leftMax) {
+                water += minOfTwoMaxes - rightHeight;
                 right--;
             }
         }
