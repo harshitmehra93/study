fde0233935dc8ba984c28622f595c08231aa50ca
jasonbourne_sysa <sysa202305@sysanalytix.com>
2026-06-06 16:58:07 +0530

    InsertInterval attempt 2

--- src/main/java/study/neetcode/interview/intervals/InsertInterval.java
+++ src/main/java/study/neetcode/interview/intervals/InsertInterval.java
@@ -29,7 +29,6 @@ Output = [[1,2],[3,10],[12,16]]
  */
 public class InsertInterval {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        if (intervals.length == 0) return new int[][] {newInterval};
         List<int[]> result = new ArrayList<>();
         int i = 0;
 
@@ -40,7 +39,7 @@ public class InsertInterval {
 
         result.add(newInterval);
 
-        while (i < intervals.length && newInterval[1] >= intervals[i][0]) {
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
             newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
             newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             i++;
@@ -50,6 +49,7 @@ public class InsertInterval {
             result.add(intervals[i]);
             i++;
         }
+
         return result.toArray(new int[result.size()][]);
     }
 }
