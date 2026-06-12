1f59441cdc7e53c5c02f2417a01fe38ecae71e76
jasonbourne_sysa <sysa202305@sysanalytix.com>
2026-06-12 05:54:01 +0530

    ValidParentheses

--- src/main/java/study/neetcode/interview/stack/ValidParentheses.java
+++ src/main/java/study/neetcode/interview/stack/ValidParentheses.java
@@ -1,22 +1,22 @@
 package study.neetcode.interview.stack;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class ValidParentheses {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> stack = new ArrayDeque<>();
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
                 continue;
             }
             if (c == ')' || c == '}' || c == ']') {
-                if (stack.empty() || stack.peek() != isMatchingOpeningBrace(c)) return false;
+                if (stack.isEmpty() || stack.peek() != isMatchingOpeningBrace(c)) return false;
                 stack.pop();
             }
         }
-        if (!stack.empty()) return false;
-        return true;
+        return stack.isEmpty();
     }
 
     private Character isMatchingOpeningBrace(char c) {
