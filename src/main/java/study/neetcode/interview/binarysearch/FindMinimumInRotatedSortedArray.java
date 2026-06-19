bcc75ee205cb01d1e921c7267ff4013ced588bcd
jasonbourne_sysa <sysa202305@sysanalytix.com>
2026-06-19 19:32:00 +0530

    TimeBasedKeyValueStore

--- src/main/java/study/neetcode/interview/binarysearch/FindMinimumInRotatedSortedArray.java
+++ src/main/java/study/neetcode/interview/binarysearch/FindMinimumInRotatedSortedArray.java
@@ -16,15 +16,15 @@ public class FindMinimumInRotatedSortedArray {
         if (nums.length == 0) return -1;
         if (nums.length == 1) return nums[0];
         int low = 0;
-        int high = nums.length-1;
+        int high = nums.length - 1;
 
-        while (low<high){
+        while (low < high) {
             int mid = low + (high - low) / 2;
 
             if (nums[mid] > nums[high]) {
-                low=mid+1;
+                low = mid + 1;
             } else {
-                high=mid;
+                high = mid;
             }
         }
 
