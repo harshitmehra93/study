307c713b01e4000247471ce089ec30dbb49fcded
jasonbourne_sysa <sysa202305@sysanalytix.com>
2026-07-19 03:03:33 +0530

    LRUCache

--- src/main/java/study/interview/linkedlist/LRUCache.java
+++ src/main/java/study/interview/linkedlist/LRUCache.java
@@ -2,7 +2,6 @@ package study.interview.linkedlist;
 
 import java.util.HashMap;
 import java.util.Map;
-import study.interview.commons.LruNode;
 
 /*
 LRU Cache
@@ -19,82 +18,76 @@ If inserting a new key exceeds capacity, evict the least recently used key.
 Both get and put must run in O(1) average time.
  */
 public class LRUCache {
-    LruNode head;
-    LruNode tail;
-    int size;
+
+    ListNode head;
+    ListNode tail;
+
     int capacity;
-    Map<Integer, LruNode> nodes;
+    Map<Integer, ListNode> map;
 
     public LRUCache(int capacity) {
+        head = new ListNode(null);
+        tail = new ListNode(null);
+        head.next = tail;
+        tail.prev = head;
         this.capacity = capacity;
-        size = 0;
-        nodes = new HashMap<>();
+        map = new HashMap<>();
     }
 
     public int get(Integer key) {
-        if (!nodes.containsKey(key)) return -1;
-        LruNode node = nodes.get(key);
-        moveNodeToHead(node);
-        return node.val;
+        if (!map.containsKey(key)) return -1;
+        var node = map.get(key);
+        moveToTail(node);
+        return node.val.value();
     }
 
     public void put(int key, int value) {
-        if (nodes.containsKey(key)) {
-            LruNode node = nodes.get(key);
-            node.val = value;
-            moveNodeToHead(node);
-            return;
-        }
-        if (size == capacity) {
-            removeLru();
-        }
-        var node = new LruNode(key, value);
-        nodes.put(key, node);
-        addToHead(node);
-    }
-
-    private void removeLru() {
-        if (size == 1) {
-            nodes.clear();
-            head = tail = null;
-            size--;
-            return;
-        }
-        nodes.remove(tail.key);
-        var secondLast = tail.next;
-        secondLast.previous = null;
-        tail = secondLast;
-        size--;
-    }
-
-    void moveNodeToHead(LruNode node) {
-        if (size == 1) return;
-        if (node == head) return;
-        if (node == tail) {
-            node.next.previous = null;
-            tail = node.next;
+        if (!map.containsKey(key)) {
+            removeFromHeadIfRequired();
+            var node = new ListNode(new KeyValue(key, value));
+            map.put(key, node);
+            addToTail(node);
         } else {
-            node.next.previous = node.previous;
-            node.previous.next = node.next;
+            var node = map.get(key);
+            node.val = new KeyValue(key, value);
+            moveToTail(node);
         }
-        head.next = node;
-        node.previous = head;
-        node.next = null;
-
-        head = node;
     }
 
-    private void addToHead(LruNode node) {
-        if (size == 0) {
-            head = tail = node;
-            size++;
-            return;
-        }
-        head.next = node;
-        node.previous = head;
-        node.next = null;
-
-        head = node;
-        size++;
+    private void moveToTail(ListNode node) {
+        detachNode(node);
+        addToTail(node);
     }
+
+    private void detachNode(ListNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void addToTail(ListNode newNode) {
+        newNode.next = tail;
+        newNode.prev = tail.prev;
+        tail.prev.next = newNode;
+        tail.prev = newNode;
+    }
+
+    private void removeFromHeadIfRequired() {
+        if (map.size() < capacity) return;
+        var node = head.next;
+        node.next.prev = head;
+        head.next = node.next;
+        map.remove(node.val.key());
+    }
+
+    public static class ListNode {
+        KeyValue val;
+        ListNode next;
+        ListNode prev;
+
+        ListNode(KeyValue val) {
+            this.val = val;
+        }
+    }
+
+    record KeyValue(int key, int value) {}
 }
