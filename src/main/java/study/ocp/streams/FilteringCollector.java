c3449283e05b47912f8a6037ce663229bf3fb667
jasonbourne_sysa <sysa202305@sysanalytix.com>
2026-09-04 19:36:45 +0530

    lld and ocp

--- src/main/java/study/ocp/streams/FilteringCollector.java
+++ src/main/java/study/ocp/streams/FilteringCollector.java
@@ -7,7 +7,7 @@ class FilteringCollector{
 		// Stream<String> s = Stream.of("1","2","3","4","5","6","7","8","9");
 		Stream<Integer> s = Stream.of(1,2,3,4,5,6,7,8,9);
 		// System.out.println(s.collect(Collectors.joining(",")));
-		System.out.println(s.collect(Collectors.filtering((a)->a%2==0),Collectors.counting()));
+//		System.out.println(s.collect(Collectors.filtering((a)->a%2==0),Collectors.counting()));
 
 
 	}
